79d5b566d88621f0da4cdc888a41b6971927998c
febyproject <febynurminasari@gmail.com>
2023-06-12 16:43:05 +0800

    Membuat class Matematika Beraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hitung;
 
-/**
- *
- * @author HP
- */
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika feby = new Matematika(7,3);
+        
+        System.out.println("Hasil Penjumlahan : "+feby.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+feby.setPengurangan());
+        System.out.println("Hasil Perkalian : "+feby.setPerkalian());
+        System.out.println("Hasil Pembagian : "+feby.setPembagian());
+    }
     
 }
